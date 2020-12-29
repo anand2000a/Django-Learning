137d580be3bf4abf1c3b59076be4257443fb8f2e
anand2000a <rahulanandyadav2000@gmail.com>
2020-12-29 23:44:22 +0530

    portfolio-structure

--- portfolio-project/portfolio/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.db.models.base import ModelState
-
-class Project(models.Model):
-    title = models.CharField(max_length=100,blank=True)
-    description=models.CharField(max_length=200)
-    image = models.ImageField(upload_to='portfolio/images/')
-    url=models.URLField(blank=True)
